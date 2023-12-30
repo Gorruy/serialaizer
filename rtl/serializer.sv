cc8eca096c8b81fe0d9ac7356f0864fd68662c84
gorruy <vladimirmimikin@gmail.com>
2023-12-30 08:09:46 +0300

    formatted all Class and task moved inside module

--- rtl/serializer.sv
+++ rtl/serializer.sv
@@ -1,73 +1,91 @@
-module serializer (
-    input  logic        clk_i,
-    input  logic        srst_i,
-    input  logic [15:0] data_i,
-    input  logic [ 3:0] data_mod_i,
-    input  logic        data_val_i,
-    output logic        ser_data_o,
-    output logic        ser_data_val_o,
-    output logic        busy_o
+module serializer #(
+  parameter DATA_BUS_WIDTH = 16;
+  parameter DATA_MOD_WIDTH = 4;
+)(
+  input  logic                        clk_i,
+  input  logic                        srst_i,
+
+  input  logic [DATA_BUS_WIDTH - 1:0] data_i,
+  input  logic [DATA_MOD_WIDTH - 1:0] data_mod_i,
+  input  logic                        data_val_i,
+
+  output logic                        ser_data_o,
+  output logic                        ser_data_val_o,
+  output logic                        busy_o
 );
 
-  enum logic [1:0] {
-    IDLE_S,
-    WORK_S
-  }
-      state, next_state;
+  enum logic [1:0] { IDLE_S,
+                     WORK_S } state, next_state;
 
   logic [ 3:0] counter;
   logic [ 3:0] final_index;
   logic [15:0] data_buf;
 
-  always_ff @(posedge clk_i) begin
-    if (srst_i) state <= IDLE_S;
-    else state <= next_state;
-  end
+  always_ff @( posedge clk_i ) 
+    begin
+      if ( srst_i ) 
+        state <= IDLE_S;
+      else 
+        state <= next_state;
+    end
 
-  always_comb begin
-    next_state = state;
-    case (state)
-      IDLE_S: begin
-        if (data_mod_i == 1 || data_mod_i == 2) next_state = IDLE_S;
-        else if (data_val_i) next_state = WORK_S;
-        else next_state = IDLE_S;
-      end
+  always_comb 
+    begin
+      next_state = state;
+      case ( state )
+        IDLE_S: begin
+          if (data_mod_i == 1 || data_mod_i == 2) 
+            next_state = IDLE_S;
+          else if (data_val_i) 
+            next_state = WORK_S;
+          else 
+            next_state = IDLE_S;
+        end
 
-      WORK_S: begin
-        if (counter == final_index + 4'b1) next_state = IDLE_S;
-        else next_state = WORK_S;
-      end
-    endcase
-  end
+        WORK_S: begin
+          if (counter == final_index + 4'b1) 
+            next_state = IDLE_S;
+          else 
+            next_state = WORK_S;
+        end
+      endcase
+    end
 
   // Set counter and data buffer
-  always_ff @(posedge clk_i) begin
-    if (state == IDLE_S) counter <= 4'd15;
-    if (state == IDLE_S && next_state == WORK_S) begin
-      data_buf <= data_i;
-      if (!data_mod_i) final_index <= 0;
-      else final_index <= 4'd15 - data_mod_i;
-    end else if (next_state == WORK_S || state == WORK_S) counter <= counter - 4'b1;
-  end
+  always_ff @( posedge clk_i ) 
+    begin
+      if ( state == IDLE_S ) 
+        counter <= DATA_BUS_WIDTH - 1;
+      if ( state == IDLE_S && next_state == WORK_S ) begin
+        data_buf <= data_i;
+        if ( !data_mod_i ) 
+          final_index <= 0;
+        else 
+          final_index <= DATA_BUS_WIDTH - 1 - data_mod_i;
+      end 
+      else if ( next_state == WORK_S || state == WORK_S ) 
+        counter <= counter - 4'b1;
+    end
 
-  always_comb begin
-    ser_data_o     = '0;
-    ser_data_val_o = 0;
-    busy_o         = 0;
-    case (state)
-      IDLE_S: begin
-        ser_data_o     = '0;
-        ser_data_val_o = 0;
-        busy_o         = 0;
-      end
+  always_comb 
+    begin
+      ser_data_o     = '0;
+      ser_data_val_o = 0;
+      busy_o         = 0;
+      case ( state )
+        IDLE_S: begin
+          ser_data_o     = '0;
+          ser_data_val_o = 0;
+          busy_o         = 0;
+        end
 
-      WORK_S: begin
-        busy_o         = 1;
-        ser_data_val_o = 1;
-        // Msb go first
-        ser_data_o     = data_buf[counter];
-      end
-    endcase
-  end
+        WORK_S: begin
+          busy_o         = 1;
+          ser_data_val_o = 1;
+          // Msb go first
+          ser_data_o     = data_buf[counter];
+        end
+      endcase
+    end
 
 endmodule
--- tb/top_tb.sv
+++ tb/top_tb.sv
@@ -1,21 +1,8 @@
-  class RandomTransactionData;
-  rand logic [15:0] input_data;
-  rand logic [3:0] size;
-
-  constraint c { size != 1; size != 2;}
-  endclass
-
-  task display_error(input logic [15:0] in, 
-                     input logic [15:0] out,
-                     input int index);
-    for (int i = 0; i <= index; i++ ) begin
-      $display("expected values:%b, result value:%b till %d", in, out, index);
-    end
-  endtask
-
 module top_tb;
 
-  parameter RUNS = 100;
+  parameter NUMBER_OF_TEST_RUNS = 100;
+  parameter DATA_BUS_WIDTH      = 16;
+  parameter DATA_MOD_WIDTH      = 4;
 
   bit clk;
   logic srst;
@@ -24,70 +11,87 @@ module top_tb;
   logic ser_data_val;
   logic busy;
 
-  logic [15:0] data;
-  logic [3:0] data_mod;
+  logic [DATA_BUS_WIDTH - 1:0] data;
+  logic [DATA_MOD_WIDTH - 1:0] data_mod;
   logic data_val;
 
-  logic [15:0] output_test_data = '0;
+  logic [DATA_BUS_WIDTH - 1:0] output_test_data = '0;
 
   // flag to indicate if there is an error
   bit test_succeed;
 
-  serializer DUT (
-      .clk_i         (clk),
-      .srst_i        (srst),
-      .ser_data_o    (ser_data),
-      .ser_data_val_o (ser_data_val),
-      .busy_o        (busy),
-      .data_i        (data),
-      .data_val_i    (data_val),
-      .data_mod_i    (data_mod)
+  serializer #(
+    .DATA_BUS_WIDTH ( DATA_BUS_WIDTH ),
+    .DATA_MOD_WIDTH ( DATA_MOD_WIDTH ),
+  ) DUT ( 
+    .clk_i          ( clk            ),
+    .srst_i         ( srst           ),
+    .ser_data_o     ( ser_data       ),
+    .ser_data_val_o ( ser_data_val   ),
+    .busy_o         ( busy           ),
+    .data_i         ( data           ),
+    .data_val_i     ( data_val       ),
+    .data_mod_i     ( data_mod       )
   );
 
-  initial
-    forever 
-      #5 clk = !clk;
+  class RandomTransactionData;
+    rand logic [DATA_BUS_WIDTH - 1:0] input_data;
+    rand logic [DATA_MOD_WIDTH - 1:0] size;
 
-    default clocking cb @ (posedge clk);
-    endclocking
+    constraint c {
+      size != 1;
+      size != 2;
+    }
+  endclass
+
+  task display_error(
+    input logic [DATA_BUS_WIDTH - 1:0] in, 
+    input logic [DATA_BUS_WIDTH - 1:0] out, 
+    input int                          index
+  );
+    for ( int i = 0; i <= index; i++ )
+      $display( "expected values:%b, result value:%b till %d", in, out, index );
+  endtask
+
+  initial forever #5 clk = !clk;
+
+  default clocking cb @( posedge clk );
+  endclocking
 
   initial begin
     RandomTransactionData tr;
     srst <= 1;
     #10;
-    srst <= 0;
+    srst         <= 0;
     test_succeed <= 1;
-    $display("Tests started!");
+    $display( "Tests started!" );
 
-    for (int i = 0; i < RUNS; i++)
-    begin
+    for ( int i = 0; i < NUMBER_OF_TEST_RUNS; i++ ) begin
       tr = new();
       tr.randomize();
 
-      data <= tr.input_data;
+      data     <= tr.input_data;
       data_val <= 1;
       data_mod <= tr.size;
       // Wait for DUT to read data at posedge
-      @(posedge clk);
+      @( posedge clk );
 
-      for (int i = 0; i < (tr.size != 4'b0 ? tr.size: 15); i++)
-      begin
+      for ( int i = 0; i < (tr.size != 0 ? tr.size : DATA_BUS_WIDTH - 1); i++ ) begin
         // wait for next posedge to read data
-        @(posedge clk);
-        if (ser_data_val)
-          output_test_data[15 - i] <= ser_data;
-          if (ser_data != tr.input_data[15 - i])
-          begin
-            test_succeed <= 0;
-            display_error(tr.input_data, output_test_data, i);
-          end
+        @( posedge clk) ;
+        if ( ser_data_val ) 
+          output_test_data[DATA_BUS_WIDTH - 1 - i] <= ser_data;
+        if ( ser_data != tr.input_data[DATA_BUS_WIDTH - 1 - i] ) begin
+          test_succeed <= 0;
+          display_error(tr.input_data, output_test_data, i);
+        end
       end
-      end
-      if (test_succeed) $display("Tests completed successfully!");
-      else $display("Tests failed!");
-      $stop();
+    end
+    if ( test_succeed ) 
+      $display( "Tests completed successfully!" );
+    else 
+      $display( "Tests failed!" );
+    $stop();
   end
-
-
 
 endmodule
