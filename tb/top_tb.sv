0e93145da461bf86863f703eb95ea097535d71fb
gorruy <vladimirmimikin@gmail.com>
2024-01-05 00:24:30 +0300

    tb

--- tb/top_tb.sv
+++ tb/top_tb.sv
@@ -4,21 +4,25 @@ module top_tb;
   parameter DATA_BUS_WIDTH      = 16;
   parameter DATA_MOD_WIDTH      = 4;
 
-  bit clk;
-  logic srst;
-
-  logic ser_data;
-  logic ser_data_val;
-  logic busy;
+  bit                          clk;
+  logic                        srst;
 
   logic [DATA_BUS_WIDTH - 1:0] data;
   logic [DATA_MOD_WIDTH - 1:0] data_mod;
-  logic data_val;
+  logic                        data_val;
 
-  logic [DATA_BUS_WIDTH - 1:0] output_test_data = '0;
+  logic                        ser_data;
+  logic                        ser_data_val;
+  logic                        busy;
+
 
   // flag to indicate if there is an error
   bit test_succeed;
+
+  initial forever #5 clk = !clk;
+
+  default clocking cb @( posedge clk );
+  endclocking
 
   serializer #(
     .DATA_BUS_WIDTH ( DATA_BUS_WIDTH ),
@@ -34,15 +38,9 @@ module top_tb;
     .data_mod_i     ( data_mod       )
   );
 
-  class RandomTransactionData;
-    rand logic [DATA_BUS_WIDTH - 1:0] input_data;
-    rand logic [DATA_MOD_WIDTH - 1:0] size;
-
-    constraint c {
-      size != 1;
-      size != 2;
-    }
-  endclass
+  mailbox #( logic [DATA_BUS_WIDTH - 1:0] ) input_data = new();
+  mailbox #( logic [DATA_MOD_WIDTH - 1:0] ) size = new();
+  mailbox #( logic [DATA_BUS_WIDTH - 1:0] ) output_data = new();
 
   task display_error(
     input logic [DATA_BUS_WIDTH - 1:0] in, 
@@ -53,47 +51,115 @@ module top_tb;
       $display( "expected values:%b, result value:%b till %d", in, out, index );
   endtask
 
-  initial forever #5 clk = !clk;
+  function void compare_data( mailbox #( logic [DATA_BUS_WIDTH - 1:0]) input_data,
+                              mailbox #( logic [DATA_MOD_WIDTH - 1:0]) size,
+                              mailbox #( logic [DATA_BUS_WIDTH - 1:0]) output_data
+                            );
+    
+    for ( int i = 0; i < size; i++ ) begin
+      if ( input_data[i] != output_data[i] )
+        display_error( input_data, output_data, i );
+    end
+    
+  endfunction
 
-  default clocking cb @( posedge clk );
-  endclocking
+  task generate_transaction ( mailbox #( logic [DATA_BUS_WIDTH - 1:0]) input_data,
+                              mailbox #( logic [DATA_MOD_WIDTH - 1:0]) size
+                            );
+    
+    logic [DATA_BUS_WIDTH - 1:0] data_to_send;
+    logic [DATA_MOD_WIDTH - 1:0] size_to_send;
+
+    data_to_send = $urandom_range( DATA_BUS_WIDTH**2 - 1, 0 );
+    size_to_send = $urandom_range( DATA_MOD_WIDTH**2 - 1, 0 );
+
+    input_data.put(data_to_send);
+    size.put(size_to_send);
+
+  endtask
+
+  task send_data ( mailbox #( logic [DATA_BUS_WIDTH - 1:0]) input_data,
+                   mailbox #( logic [DATA_MOD_WIDTH - 1:0]) size
+                 );
+    
+    logic [DATA_BUS_WIDTH - 1:0] data_to_send;
+    logic [DATA_BUS_WIDTH - 1:0] size_to_send;
+
+    input_data.get( data_to_send );
+    size.get( size_to_send );
+
+    data     <= data_to_send;
+    data_mod <= size;
+    data_val <= 1'b1;
+    #10
+    data     <= '0;
+    data_mod <= '0;
+    data_val <= 1'b0;    
+
+  endtask
+
+  task read_data ( mailbox #( logic [DATA_BUS_WIDTH - 1:0]) output_data,
+                   mailbox #( logic [DATA_MOD_WIDTH - 1:0]) size 
+                 );
+    
+    logic [DATA_BUS_WIDTH - 1:0] recieved_data;
+    logic [DATA_MOD_WIDTH - 1:0] tr_size;
+    
+    recieved_data <= '0;
+    size.get(tr_size);
+
+    for ( int i = 0; i < tr_size; i++ ) begin
+      @( posedge clk_i );
+      if ( ser_data_val == 1'b1 )
+        recieved_data[i] <= ser_data;
+    end
+
+    output_data.put(recieved_data);
+
+  endtask
+
+
 
   initial begin
-    RandomTransactionData tr;
-    srst <= 1;
-    #10;
-    srst         <= 0;
-    test_succeed <= 1;
-    $display( "Tests started!" );
-
-    for ( int i = 0; i < NUMBER_OF_TEST_RUNS; i++ ) begin
-      tr = new();
-      tr.randomize();
-
-      data     <= tr.input_data;
-      data_val <= 1;
-      data_mod <= tr.size;
-      // Wait for DUT to read data at posedge
-      @( posedge clk );
-      data_val <= 0;
-
-      for ( int i = 0; i < ( tr.size != 0 ? tr.size : DATA_BUS_WIDTH ); i++ ) begin
-        // wait for next posedge to read data
-        @( posedge clk) ;
-        if ( ser_data_val ) 
-          output_test_data[DATA_BUS_WIDTH - 1 - i] <= ser_data;
-        if ( ser_data != tr.input_data[DATA_BUS_WIDTH - 1 - i] ) begin
-          test_succeed <= 0;
-          display_error(tr.input_data, output_test_data, i);
-        end
-      end
-      output_test_data <= '0;
-    end
-    if ( test_succeed ) 
-      $display( "Tests completed successfully!" );
-    else 
-      $display( "Tests failed!" );
-    $stop();
+    
   end
+
+  // initial begin
+  //   RandomTransactionData tr;
+  //   srst <= 1;
+  //   #10;
+  //   srst         <= 0;
+  //   test_succeed <= 1;
+  //   $display( "Tests started!" );
+
+  //   for ( int i = 0; i < NUMBER_OF_TEST_RUNS; i++ ) begin
+  //     tr = new();
+  //     tr.randomize();
+
+  //     data     <= tr.input_data;
+  //     data_val <= 1;
+  //     data_mod <= tr.size;
+  //     // Wait for DUT to read data at posedge
+  //     @( posedge clk );
+  //     data_val <= 0;
+
+  //     for ( int i = 0; i < ( tr.size != 0 ? tr.size : DATA_BUS_WIDTH ); i++ ) begin
+  //       // wait for next posedge to read data
+  //       @( posedge clk) ;
+  //       if ( ser_data_val ) 
+  //         output_test_data[DATA_BUS_WIDTH - 1 - i] <= ser_data;
+  //       if ( ser_data != tr.input_data[DATA_BUS_WIDTH - 1 - i] ) begin
+  //         test_succeed <= 0;
+  //         display_error(tr.input_data, output_test_data, i);
+  //       end
+  //     end
+  //     output_test_data <= '0;
+  //   end
+  //   if ( test_succeed ) 
+  //     $display( "Tests completed successfully!" );
+  //   else 
+  //     $display( "Tests failed!" );
+  //   $stop();
+  // end
 
 endmodule
